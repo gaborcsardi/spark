7d3865fcc3e4226859c7d0439df20035cd36f3a2
Gabor Csardi <csardi.gabor@gmail.com>
2014-10-13 15:05:27 -0400

    Add tests, #1

---
+++ tests/testthat/test-spark.r
@@ -0,0 +1,34 @@
+
+context("Spark lines")
+
+test_that("spark_line works", {
+
+  tests <- list(
+    list(c(0,30,55,80,33,150), '▁▂▃▅▂█'), # 1
+    list(c(1, 5, 22, 13, 53), '▁▁▄▂█'),   # 2
+    list(0:7, '▁▂▃▄▅▆▇█'),                # 3
+    list(c(0, NaN, 7), '▁ █'),            # 4
+    list(c(1,9,6,2,1,3,6,5,3,7,6,9), '▁█▅▁▁▂▅▄▂▆▅█') # 5
+  )
+
+  sapply(seq_along(tests), function(i) {
+    expect_equal(spark_line(tests[[i]][[1]]),
+                 tests[[i]][[2]],
+                 info = paste("#", i))
+  })
+})
+
+test_that("spark_line for constant data is good", {
+
+  expect_equal(spark_line(rep(10, 5)), '▄▄▄▄▄')
+
+})
+
+test_that("spark_line can scale horizontally", {
+
+  csl <- spark_line(10)
+  csl80 <- paste(rep(csl, getOption("width")), collapse = "")
+  expect_equal(spark_line(rep(10, 100), width = "auto"), csl80)
+  expect_equal(spark_line(rep(10, 100), width = "screen"), csl80)
+
+})
