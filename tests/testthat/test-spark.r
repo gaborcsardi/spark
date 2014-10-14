3fa9cbd1f0f7029314af1f027c13eebcc3fcd903
Gabor Csardi <csardi.gabor@gmail.com>
2014-10-14 09:38:46 -0400

    Fix tests for fallback

--- tests/testthat/test-spark.r
+++ tests/testthat/test-spark.r
@@ -5,14 +5,18 @@ spark <- function(...) {
   spark::spark(..., print = FALSE)
 }
 
+`%or%` <- function(lhs, rhs) {
+  if (is_utf8()) lhs else rhs
+}
+
 test_that("spark_line works", {
 
   tests <- list(
-    list(c(0,30,55,80,33,150), '▁▂▃▅▂█'), # 1
-    list(c(1, 5, 22, 13, 53), '▁▁▄▂█'),   # 2
-    list(0:7, '▁▂▃▄▅▆▇█'),                # 3
-    list(c(0, NaN, 7), '▁ █'),            # 4
-    list(c(1,9,6,2,1,3,6,5,3,7,6,9), '▁█▅▁▁▂▅▄▂▆▅█') # 5
+    list(c(0,30,55,80,33,150), '▁▂▃▅▂█' %or% '__~~_^'), # 1
+    list(c(1, 5, 22, 13, 53), '▁▁▄▂█' %or% '__~_^'),    # 2
+    list(0:7, '▁▂▃▄▅▆▇█' %or% '___~~^^^'),              # 3
+    list(c(0, NaN, 7), '▁ █' %or% '_ ^'),               # 4
+    list(c(1,9,6,2,1,3,6,5,3,7,6,9), '▁█▅▁▁▂▅▄▂▆▅█' %or% '_^~___~~_^~^') # 5
   )
 
   sapply(seq_along(tests), function(i) {
@@ -24,7 +28,7 @@ test_that("spark_line works", {
 
 test_that("spark_line for constant data is good", {
 
-  expect_equal(spark(rep(10, 5)), '▄▄▄▄▄')
+  expect_equal(spark(rep(10, 5)), '▄▄▄▄▄' %or% '~~~~~')
 
 })
 
