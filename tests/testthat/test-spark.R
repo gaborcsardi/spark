867e629ba1d4820b086fd6d665a47ae807113883
Gábor Csárdi <csardi.gabor@gmail.com>
2017-05-21 18:49:21 +0100

    More test cases

--- tests/testthat/test-spark.R
+++ tests/testthat/test-spark.R
@@ -27,3 +27,48 @@ test_that("spark_line for constant data is good", {
   expect_equal(unclass(spark(rep(10, 5))), '▄▄▄▄▄' %or% '~~~~~')
 
 })
+
+test_that("printing", {
+  s <- spark(1:10)
+  expect_output(print(s), s)
+})
+
+test_that("auto width", {
+  s <- spark(1:10, width = "auto")
+  expect_equal(nchar(s), 10)
+
+  s <- spark(1:1000, width = "auto")
+  expect_equal(nchar(s), getOption("width"))
+})
+
+test_that("constant data", {
+  s <- spark(rep(1, 10))
+  ss <- strsplit(s, "")[[1]]
+  expect_equal(length(ss), 10)
+  expect_equal(length(unique(ss)), 1)
+  expect_true(ss[1] %in% spark_ticks())
+
+  s <- spark(rep(0, 10))
+  ss <- strsplit(s, "")[[1]]
+  expect_equal(length(ss), 10)
+  expect_equal(length(unique(ss)), 1)
+  expect_true(ss[1] %in% spark_ticks())
+})
+
+test_that("marking the max", {
+  s <- withr::with_options(
+    list(crayon.enabled = TRUE, crayon.colors = 256),
+    spark(c(1:3, 3:1), max = crayon::red)
+  )
+  expect_true(crayon::has_style(s))
+})
+
+test_that("marking the max without crayon", {
+  mockr::with_mock(
+    `is_installed` = function(...) FALSE,
+    expect_warning(
+      spark(c(1:3, 3:1), max = "red"),
+      "The .crayon. package is needed"
+    )
+  )
+})
